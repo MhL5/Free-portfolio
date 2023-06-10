bc23320a045e57eda94f7be8e7c460c2e7ccd7c6
MhL5 <sapumr@gmail.com>
2023-06-10 19:42:58 +0330

    tab logic added

--- js/script.js
+++ js/script.js
@@ -12,4 +12,32 @@ var typed = new Typed("#element", {
 // END of type effect
 
 // tabs
+$(document).ready(function () {
+  $("#tabNav1").on("click", function () {
+    $(".tab1").addClass("active");
+    $(".tab2").removeClass("active");
+    $(".tab3").removeClass("active");
+    $(".tab4").removeClass("active");
+  });
 
+  $("#tabNav2").on("click", function () {
+    $(".tab2").addClass("active");
+    $(".tab1").removeClass("active");
+    $(".tab3").removeClass("active");
+    $(".tab4").removeClass("active");
+  });
+
+  $("#tabNav3").on("click", function () {
+    $(".tab3").addClass("active");
+    $(".tab1").removeClass("active");
+    $(".tab2").removeClass("active");
+    $(".tab4").removeClass("active");
+  });
+
+  $("#tabNav4").on("click", function () {
+    $(".tab4").addClass("active");
+    $(".tab1").removeClass("active");
+    $(".tab3").removeClass("active");
+    $(".tab2").removeClass("active");
+  });
+});
