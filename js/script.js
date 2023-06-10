fd4062f38c15a1f096aa543ef226da8beba205f6
MhL5 <sapumr@gmail.com>
2023-06-10 18:19:44 +0330

    initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Type effect
+var typed = new Typed("#element", {
+  strings: [
+    "very happy to see you here!",
+    "Mohammad Hosein Lashani.",
+    "<i>a</i> Developer.",
+    "a Frontend junior.",
+  ],
+  typeSpeed: 250,
+  loop: true, // Enable infinite loop
+});
+// END of type effect
+
+// tabs
+
